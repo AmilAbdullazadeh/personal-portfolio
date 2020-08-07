80dca427a5a8f72efb7230c85f350daef407957d
AmilAbdullazadeh <amilva@code.edu.az>
2020-08-07 13:40:20 +0000

    Remove old stuff

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w16#n!q)030#vc-mp@!%q4a*d6g@n4c1f7gkcez9_vuj)r@b+k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amiltech.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
